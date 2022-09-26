c1561740beb019b8e429ba9dd9a3b3d49168c569
IXhaka <114148381+IXhaka@users.noreply.github.com>
2022-09-26 16:27:00 +0300

    Ibm Data Science Quiz
    https://eu-de.dataplatform.cloud.ibm.com/analytics/notebooks/v2/ab9f25ed-f3bb-4f87-8d19-45d23dc4c10a/view?access_token=4aaecd1b9e314aae248727dc065e113bd115eca6681f5746c1894f24e7e6b76b

---
+++ Data Science Eren.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# ## Eren
+
+# *I am interested in data science because i want to do advanced research in the field of psychology.*
+
+# ### The following code should output Hello, World !
+
+# In[1]:
+
+
+print("Hello, World!")
+
+
+# -----
+# 
+# * item1
+# * item2
+# * item3
+# 
+# 1. item1
+# 2. item2
+# 3. item3
+# 
+# | Grocery list| Bought or N |
+# | ----------- | ----------- |
+# | Item 1      | Yes         |
+# | Item 2      | No          |
+# 
+# http://www.coursera.com
+# 
+# ![My Fav image]![Wormhole%20%5B3840%20x%202160%5D%20actually%206000x4000.jpeg](attachment:Wormhole%20%5B3840%20x%202160%5D%20actually%206000x4000.jpeg)
+# 
+# At the command prompt, type `eren`.
+# 
+# > I am a fresh Data Science student.
+# > I am really excited to learn more !!!
+# 
+# ~~this is trial~~
+# 
+# 
+
+# In[ ]:
+
+
+
+
